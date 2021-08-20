7b0fa52bca20ba23f7c1d62fa69a9671b0f381e9
Josh Don <joshdon@google.com>
2021-08-19 18:04:02 -0700

    sched: reduce sched slice for SCHED_IDLE entities
    Use a small, non-scaled min granularity for SCHED_IDLE entities, when
    competing with normal entities. This reduces the latency of getting
    a normal entity back on cpu, at the expense of increased context
    switch frequency of SCHED_IDLE entities.
    
    The benefit of this change is to reduce the round-robin latency for
    normal entities when competing with a SCHED_IDLE entity.
    
    Example: on a machine with HZ=1000, spawned two threads, one of which is
    SCHED_IDLE, and affined to one cpu. Without this patch, the SCHED_IDLE
    thread runs for 4ms then waits for 1.4s. With this patch, it runs for
    1ms and waits 340ms (as it round-robins with the other thread).
    
    Signed-off-by: Josh Don <joshdon@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20210820010403.946838-4-joshdon@google.com
    (cherry picked from commit 51ce83ed523b00d58f2937ec014b12daaad55185)
    (cherry picked from commit 03bfa1ae42f8d9893a31ce32b15fafd3c01e3402)
    (cherry picked from commit 730bc877ee746e984668c4fd7815285640b8af04)

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -242,6 +242,11 @@ static int proc_dointvec_minmax_sysadmin(struct ctl_table *table, int write,
 				void __user *buffer, size_t *lenp, loff_t *ppos);
 #endif
 
+#ifdef CONFIG_SCHED_DEBUG
+static int idle_min_granularity = 750000ULL;
+static int idle_max_granularity = 7500000ULL;
+#endif /* CONFIG_SCHED_DEBUG */
+
 static int proc_dointvec_minmax_coredump(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp, loff_t *ppos);
 #ifdef CONFIG_COREDUMP
@@ -648,6 +653,15 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_proc_update_handler,
 		.extra1		= &min_sched_granularity_ns,
 		.extra2		= &max_sched_granularity_ns,
+	},
+	{
+		.procname	= "sched_idle_min_granularity_ns",
+		.data		= &sysctl_sched_min_granularity,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_proc_update_handler,
+		.extra1		= &idle_min_granularity_ns,
+		.extra2		= &idle_max_granularity_ns,
 	},
 	{
 		.procname	= "sched_latency_ns",
